3da538f9b0d5befefcd03c8537a4cc7574358daa
kasyap <kasyapkovvuru@kgpian.iitkgp.ac.in>
2024-06-26 01:14:01 +0530

    all changes

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,23 @@
+//
+// Created by KASYAP on 26-06-2024.
+//
+
+#ifndef PATTERNDATABASE_H
+#define PATTERNDATABASE_H
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+
+#endif //PATTERNDATABASE_H
---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,30 @@
+//
+// Created by KASYAP on 26-06-2024.
+//
+
+#ifndef CORNERDBMAKER_H
+#define CORNERDBMAKER_H
+
+
+
+#include "CornerPatternDatabase.h"
+#include "../Model/RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+
+
+#endif //CORNERDBMAKER_H
